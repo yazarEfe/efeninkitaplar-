0ea7e901c9b98159e8d0a20e418e7b4128795d6b
yazarEfe <efe.berk.usta.56@gmail.com>
2026-05-02 22:49:29 +0300

    src/main.jsx dosyası eklendi
    React ve Vite için giriş dosyası src klasörüne oluşturuldu

---
+++ src/src/src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import YeniAskerSite from "../yeni-asker-site";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <YeniAskerSite />
+  </React.StrictMode>
+);
